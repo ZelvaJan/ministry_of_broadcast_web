b242c1c28fc44bef3e480b20293bb47c002cba73
ZelvaJan <petr.melicherik@seznam.cz>
2018-09-14 16:00:13 +0200

    - update seo and snow effects

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -59,7 +59,7 @@ class MainPage extends Component {
             function Snowy() {
                 var snow, arr = [];
                 var num = 600, tsc = 1, sp = 1;
-                var sc = 1.3, t = 0, mv = 15, min = 1;
+                var sc = 1.3, t = 0, mv = 15, min = 1, max = 2;
                 if (width < 800) {
                     num = 200;
                     mv = 10;
@@ -73,6 +73,8 @@ class MainPage extends Component {
                     snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
                     snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
                     snow.sp = snow.sp < min ? min : snow.sp;
+                    snow.sp = snow.sp > max ? max : snow.sp;
+                    console.log(snow.sp);
                     arr.push(snow);
                 }
                 go();
@@ -88,7 +90,8 @@ class MainPage extends Component {
                         f.t += .05;
                         f.t = f.t >= Math.PI * 2 ? 0 : f.t;
                         f.y += f.sp;
-                        f.x += Math.sin(f.t * tsc) * (f.sz * .3);
+                        //f.x += Math.sin(f.t * tsc) * (f.sz * .3) + Math.random() * 3;
+                        f.x = f.x + Math.random() * 1.5;
                         if (f.y > h + 50) f.y = -10 - Math.random() * mv;
                         if (f.x > w + mv) f.x = -mv;
                         if (f.x < -mv) f.x = w + mv;
@@ -101,6 +104,8 @@ class MainPage extends Component {
                         this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
                         this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
                         this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
+                        //this.g = $.blendColor(255,255,255, 0.5);
+
                         $.moveTo(this.x, this.y);
                         $.fillStyle = this.g;
                         $.beginPath();
